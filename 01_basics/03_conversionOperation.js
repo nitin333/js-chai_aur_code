fcf8121634862098b4b1289a59dd35a7a7b8dfee
nitin333 <35676866+nitin333@users.noreply.github.com>
2024-02-04 10:40:21 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+// console.log(typeof score)
+
+let valueinNumber = Number(score)
+// console.log(valueinNumber)
+// console.log(typeof score)
+// console.log(typeof valueinNumber)
+
+// 33 => 33
+// "33" => NaN
+// true => 1 or false => 0
+
+let isLoggedIn = 1
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+//  1 => true ; 0 => false
+//  "" => false ; "aabb" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
